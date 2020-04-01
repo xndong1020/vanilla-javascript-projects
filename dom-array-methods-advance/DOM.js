941cdf8127a8272e4e0ce615b9fbd984bd91072f
Nicole Dong <xndong1020@gmail.com>
2020-04-02 10:10:31 +1100

    code refactor

---
+++ dom-array-methods-advance/DOM.js
@@ -0,0 +1,28 @@
+// Update DOM
+function updateDOM(providedData = loadedUsers) {
+  // Clear main div.如果这里不用main.innerHTML clear main div, 那么random user会反复的重复的出现并叠加。
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+function updateSortIcon(desc = true) {
+  const btn = document.getElementById('sort');
+  const btnText = desc ? `Sort Wealth ↑` : `Sort Wealth ↓`;
+  btn.innerText = btnText;
+}
+
+function updateWealthTotal(wealth = 0) {
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = !wealth
+    ? `Please add some user`
+    : `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
---
+++ dom-array-methods-advance/script.js
@@ -0,0 +1,69 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let loadedUsers = [];
+let sortDesc = false;
+
+init();
+
+// for initial page load
+async function init() {
+  const user1 = await getRandomUser();
+  const user2 = await getRandomUser();
+  const user3 = await getRandomUser();
+  loadedUsers = [user1, user2, user3];
+  updateDOM(loadedUsers);
+}
+
+async function addUserClickHandler() {
+  const user = await getRandomUser();
+  addData(user);
+}
+
+function doubleMoneyClickHandler() {
+  if (!Array.isArray(loadedUsers) || !loadedUsers.length) return;
+  loadedUsers = doubleMoney(loadedUsers);
+  updateDOM(loadedUsers);
+}
+
+// Sort users by richest
+function sortByWealthClickHander() {
+  // toggle sort order
+  sortDesc = !sortDesc;
+  // create different comparer based on 'sortDesc' value
+  const sortFn = sortDesc
+    ? (a, b) => b.money - a.money
+    : (a, b) => a.money - b.money;
+  loadedUsers = sortUsers(loadedUsers, sortFn);
+  updateSortIcon(sortDesc);
+  updateDOM(loadedUsers);
+}
+
+// Filter only millionaires
+function showMillionairesClickHandler() {
+  loadedUsers = getUsersByWealthAmont(loadedUsers, 1000000);
+  updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealthClickHander() {
+  const wealth = getUsersTotalAmount(loadedUsers);
+  updateWealthTotal(wealth);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  loadedUsers.push(obj);
+  updateDOM();
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', addUserClickHandler);
+doubleBtn.addEventListener('click', doubleMoneyClickHandler);
+sortBtn.addEventListener('click', sortByWealthClickHander);
+showMillionairesBtn.addEventListener('click', showMillionairesClickHandler);
+calculateWealthBtn.addEventListener('click', calculateWealthClickHander);
